cfca17aee3d84c02e23f351785bb53262d7f515a
saadabban76 <saadabban76@gmail.com>
2024-01-02 23:45:47 +0530

    learned active indexing in the navbars

--- src/components/NavItem.tsx
+++ src/components/NavItem.tsx
@@ -1,21 +1,40 @@
+"use client";
+
 import React from 'react'
 import { Button } from './ui/button'
 import { PRODUCT_CATEGORIES } from '@/config'
+import { ChevronDown } from 'lucide-react';
+import { cn } from '@/lib/utils';
 
 type Category = typeof PRODUCT_CATEGORIES[number];
 
 interface NavItemProps {
     category: Category
+    handleOpen: () => void
+    isOpen: boolean
+    isAnyOpen: boolean
 }
 
-const NavItem = ({category}: NavItemProps) => {
-  return (
-      <div className='flex'>
-          <div className='relative flex items-center'>
-              <Button variant={'link'} className='gap-1.5'>{category.label}</Button>
-          </div>
-          </div>
-  )
+
+const NavItem = ({ category, isAnyOpen, handleOpen, isOpen }: NavItemProps) => {
+    // console.log('navitem : ', isOpen);
+    return (
+        <div className='flex'>
+            <div className='relative flex items-center'>
+                <Button variant={isOpen ? "secondary" : "ghost"} className='gap-1.5' onClick={handleOpen} >
+                    {category.label}
+                    <ChevronDown className={cn('h-4 w-4 transition-all text-muted-foreground', {
+                        "-rotate-180": isOpen,
+                    })} />
+                </Button>
+
+                {/* dropdown images container */}
+                <div>
+                    
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default NavItem
--- src/components/NavItems.tsx
+++ src/components/NavItems.tsx
@@ -5,25 +5,34 @@ import React, { useState } from 'react'
 import NavItem from './NavItem';
 
 const NavItems = () => {
-  const [activeIndex, setActiveIndex] = useState<null | number>(null)
+  const [activeIndex, setActiveIndex] = useState<null | number>(null); // Learnings : this will be initialized only once when the page loads and which acts as the active navbar selected functionality.
+  const isAnyOpen = activeIndex !== null; 
 
-
+  console.log('active INdex : ', activeIndex);
 
   return (
     <div className='flex gap-4 h-full'>
-      {PRODUCT_CATEGORIES.map((category, i) => {
+      {PRODUCT_CATEGORIES.map((category, i) => {        
         const handleOpen = () => {
-          if (activeIndex == i) {
+          if (activeIndex === i) {
             setActiveIndex(null);
           } else {
             setActiveIndex(i);
           }
         }
 
-        const isOpen = i === activeIndex;
+        const isOpen = i === activeIndex; // Learnings : this becomes true only if activeIndex contains the index of the navItem else it will just be false which will result in the unselected navItem
+
+        console.log(i + " = " + isOpen);
+        // console.log(i + " = " + isOpen);
 
         return (
-          <NavItem category={category} key={i} />
+          <NavItem
+            handleOpen={handleOpen}
+            isOpen={isOpen}
+            key={category.value}
+            isAnyOpen={isAnyOpen}
+            category={category} />
         );
       })}
     </div>
